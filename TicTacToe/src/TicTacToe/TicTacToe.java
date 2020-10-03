1098e40e4b4a94b91e6b48517609371ef9b84b25
Sarah Temple <sarahelisetemple@gmail.com>
2020-10-02 23:37:23 -0700

    Works on reset

--- TicTacToe/src/TicTacToe/TicTacToe.java
+++ TicTacToe/src/TicTacToe/TicTacToe.java
@@ -1,8 +1,8 @@
 package TicTacToe;
-/*
- * Name:Sarah Temple
- * Date: 
- */
+/* Name:Sarah Temple
+ * Date: October 2
+ * This should be a tictactoe game for the user to play with themselves
+ */ 
 import java.util.Scanner;
 public class TicTacToe {
 
@@ -15,51 +15,77 @@ public class TicTacToe {
 	int turn = X_TURN;
 	Scanner scanner;
 	String input = "";
+	boolean stillPlaying = true;
+	boolean askToPlayAgain = false;
 	
 	public TicTacToe() {
-		 
-		while (true) {
-			printBoard();
-			scanner = new Scanner(System.in);
-			input = scanner.nextLine();
-			if (input.length() != 2) {
-				System.out.println("Enter a letter followed by a number");
-			}
-			else if (input.charAt(0)!= 'a' &&
-					input.charAt(0)!= 'b' &&
-					input.charAt(0)!= 'c') {
-						System.out.println("Row must be a, b, or c.");
-			}
-			else if (input.charAt(1)!= '1' &&
-					input.charAt(1)!= '2' &&
-					input.charAt(1)!= '3') {
-				System.out.println("Column must be 1, 2, or 3.");
-			}
-			else {
-				int row = input.charAt(0) - 'a';
-				int column = input.charAt(1) - '1'; 
-				System.out.println(row+", "+ column);
-				if (turn == X_TURN) {
-					board[row][column] = X_MOVE;
-					turn = O_TURN;
+		while (stillPlaying == true) {
+			while (checkWin(X_MOVE) == false && checkWin(O_MOVE)==false && checkTie() == false) {
+				printBoard();
+				scanner = new Scanner(System.in);
+				input = scanner.nextLine();
+				if (input.length() != 2) {
+					System.out.println("Enter a letter followed by a number");
+				}
+				else if (input.charAt(0)!= 'a' &&
+						input.charAt(0)!= 'b' &&
+						input.charAt(0)!= 'c') {
+							System.out.println("Row must be a, b, or c.");
+				}
+				else if (input.charAt(1)!= '1' &&
+						input.charAt(1)!= '2' &&
+						input.charAt(1)!= '3') {
+					System.out.println("Column must be 1, 2, or 3.");
 				}
 				else {
-					board[row][column] = O_MOVE;
-					turn =  X_TURN;
+					int row = input.charAt(0) - 'a';
+					int column = input.charAt(1) - '1'; 
+					System.out.println(row+", "+ column);
+					if (turn == X_TURN) {
+						board[row][column] = X_MOVE;
+						turn = O_TURN;
+					}
+					else {
+						board[row][column] = O_MOVE;
+						turn =  X_TURN;
+					}
+					
 				}
-				
+			}
+			if (checkWin(X_MOVE) == true) {
+				System.out.println("X wins!!");
+				askToPlayAgain = true;
+			}
+			else if (checkWin(O_MOVE) == true) {
+				System.out.println("O wins!!");
+				askToPlayAgain = true;
+			}
+			else if (checkTie() == true) {
+				System.out.println("Its a tie!");
+				askToPlayAgain = true;
+			}
+			if (askToPlayAgain == true) {
+				System.out.println("Do you want to play again? Type Y for yes and N for no.");
+				scanner = new Scanner(System.in);
+				String yesno = scanner.nextLine();
+				if (yesno == "Y") {
+					askToPlayAgain = false;
+					stillPlaying = true;
+				}
+				else if (yesno =="N") {
+					askToPlayAgain = false;
+					stillPlaying = false;
+				}
+
 			}
 		}
 	}
-
 	
 	public static void main(String[] args) {
 		new TicTacToe();
 	}
 
-	public void printBoard() {
-		//board[1][2] = X_MOVE; -- tests
-		//board[0][0] = O_MOVE;
+	public void printBoard() {// This is where we 
 		System.out.println("\t1\t2\t3");
 		for (int row = 0; row < board.length; row++) {
 			String output = (char)('a'+row)+"\t";
@@ -78,6 +104,44 @@ public class TicTacToe {
 			
 			
 		}
+	}
+	public boolean checkWin(int player) {
+		if (board[0][0] == player && board[1][0] == player && board[2][0] == player) {
+			return true;
+		}
+		if (board[0][1] == player && board[1][1] == player && board[2][1] == player) {
+			return true;
+		}
+		if (board[0][2] == player && board[1][2] == player && board[2][2] == player) {
+			return true;
+		}
+		if (board[0][0] == player && board[0][1] == player && board[0][2] == player) {
+			return true;
+		}
+		if (board[1][0] == player && board[1][1] == player && board[1][2] == player) {
+			return true;
+		}
+		if (board[2][0] == player && board[2][1] == player && board[2][2] == player) {
+			return true;
+		}
+		if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+			return true;
+		}
+		if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+			return true;
+		}
+		
+		return false;
+	}
+	public boolean checkTie() {
+		for(int row = 0; row < board.length; row++) {
+			for (int column = 0; column < board.length; column++) {
+				if (board[row][column] == BLANK) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
 
